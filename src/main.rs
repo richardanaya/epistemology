59fc0b57cca948656b607db4016b7fe884c8c42f
Richard Anaya <richard.anaya@gmail.com>
2024-01-02 18:22:21 -0800

    fixes

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use clap::Parser;
 use futures::StreamExt;
 use serde::Deserialize;
+use std::fs;
 use std::io::{BufRead, BufReader};
+use std::path::PathBuf;
 use std::process::{Command, Stdio};
 use std::thread;
 use tokio::sync::mpsc;
 use tokio_stream::wrappers::UnboundedReceiverStream;
-use clap::{Parser};
-use std::path::PathBuf;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
-
     /// Sets a custom config file
     #[arg(short, value_name = "GGUF_MODEL")]
     model: PathBuf,
@@ -21,9 +21,9 @@ struct Cli {
     #[arg(short, value_name = "LLAMMA_CPP_MAIN_EXE_PATH")]
     path: PathBuf,
 
-     /// Sets a custom config file
-     #[arg(short, value_name = "UI_PATH")]
-     ui: Option<PathBuf>,
+    /// Sets a custom config file
+    #[arg(short, value_name = "UI_PATH")]
+    ui: Option<PathBuf>,
 }
 
 #[derive(Deserialize)]
@@ -65,12 +65,27 @@ async fn handle_post(data: web::Data<AppState>, body: String) -> impl Responder 
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let cli:Cli = Cli::parse();
+    let cli: Cli = Cli::parse();
 
-    let app_data = web::Data::new(AppState {bin_path:cli.path.display().to_string(), model_path:cli.model.display().to_string() });
+    let app_data = web::Data::new(AppState {
+        bin_path: match fs::canonicalize(cli.path) {
+            Ok(full_path) => full_path.display().to_string(),
+            Err(err) => return Err(std::io::Error::new(std::io::ErrorKind::Other, err)),
+        },
+        model_path: match fs::canonicalize(cli.model) {
+            Ok(full_path) => full_path.display().to_string(),
+            Err(err) => return Err(std::io::Error::new(std::io::ErrorKind::Other, err)),
+        },
+    });
 
     if let Some(ui) = &cli.ui {
-        println!("Serving UI on http://localhost:8080/ui/ from {}", ui.display().to_string());
+        println!(
+            "Serving UI on http://localhost:8080/ui/ from {}",
+            match fs::canonicalize(ui) {
+                Ok(full_path) => full_path.display().to_string(),
+                Err(err) => return Err(std::io::Error::new(std::io::ErrorKind::Other, err)),
+            }
+        );
     }
     println!(
         r#"Listening with GET and POST on http://localhost:8080/text-completion
@@ -78,17 +93,27 @@ Examples:
     * http://localhost:8080/text-completion?prompt=hello
     * curl -X POST -d "hello" http://localhost:8080/text-completion"#
     );
-    
 
     HttpServer::new(move || {
-        let mut a =App::new().app_data(app_data.clone()).service(
+        let mut a = App::new().app_data(app_data.clone()).service(
             web::resource("/text-completion")
                 .route(web::get().to(handle_get))
                 .route(web::post().to(handle_post)),
         );
 
         if let Some(ui_path) = &cli.ui {
-            a = a.service(actix_files::Files::new("/ui", ui_path.display().to_string()).index_file("index.html"));
+            a = a.service(
+                actix_files::Files::new(
+                    "/ui",
+                    match fs::canonicalize(ui_path) {
+                        Ok(full_path) => full_path.display().to_string(),
+                        Err(err) => {
+                            panic!("Failed to serve UI: {}", err)
+                        }
+                    },
+                )
+                .index_file("index.html"),
+            );
         }
 
         a
@@ -98,11 +123,14 @@ Examples:
     .await
 }
 
-fn execute_llm(bin_path: &str, model_path: &str, prompt: String, sender: mpsc::UnboundedSender<String>) {
+fn execute_llm(
+    bin_path: &str,
+    model_path: &str,
+    prompt: String,
+    sender: mpsc::UnboundedSender<String>,
+) {
     let prompt = format!("\"{}\"", prompt);
     let vec_cmd = vec![
-        "/C",
-        bin_path,
         "-m",
         model_path,
         "-n",
@@ -114,7 +142,7 @@ fn execute_llm(bin_path: &str, model_path: &str, prompt: String, sender: mpsc::U
         prompt.as_str(),
     ];
 
-    let mut child = Command::new("cmd")
+    let mut child = Command::new(bin_path)
         .args(&vec_cmd)
         .stdout(Stdio::piped())
         .spawn()
